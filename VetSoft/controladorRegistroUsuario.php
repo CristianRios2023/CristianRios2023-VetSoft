7618fdcd4bddf4c0101529680a137538f379c110
CristianRios2023 <133795759+CristianRios2023@users.noreply.github.com>
2023-11-18 23:28:49 -0300

    Update controladorRegistroUsuario.php
    Borrado de textos demás y cambio de Mensaje

--- VetSoft/controladorRegistroUsuario.php
+++ VetSoft/controladorRegistroUsuario.php
@@ -2,11 +2,9 @@
 
 include("conexionBD.php");
 if (isset($_POST['enviar'])) {
-	echo"if 1";
     if (strlen($_POST['nombre']) >= 1 &&
 	strlen($_POST['usuario']) >=1 &&
 	strlen($_POST['contraseña']) >=1) {
-        echo"if 2";
 		$name =trim($_POST['nombre']);
 		$apellido =trim($_POST['apellido']);
 		$usuario=trim($_POST['usuario']);
@@ -16,11 +14,11 @@ if (isset($_POST['enviar'])) {
 		$resultado = mysqli_query($conexion,$consulta);
 		if ($resultado) {
 			?>
-                <h3 class="ok"> Te inscribiste Bien!!</h3>
+                <h3 class="ok"> ¡¡Te Registraste Correctamente!!</h3>
 			<?php
 		}else {
 			?>
-                <h3 class="bad"> Algo no salio bien </h3>
+                <h3 class="bad"> Upps! Algo no salio bien </h3>
 			<?php
 		}
 	}	else {
